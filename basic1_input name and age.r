32f37802849bf91b0f4324661f2399b55df9f942
Sankalp Swarup <80690415+sankalp20@users.noreply.github.com>
2021-06-26 13:54:07 +0530

    Create basic1_input name and age.r

---
+++ basic1_input name and age.r
@@ -0,0 +1,9 @@
+name = readline(prompt="Input your name: ")
+age =  readline(prompt="Input your age: ")
+print(paste("My name is",name, "and I am",age ,"years old."))
+print(R.version.string)
+
+Input your name: sankalp
+Input your age: 20
+[1] "My name is sankalp and I am 20 years old."
+[1] "R version 4.0.5 (2021-03-31)"
